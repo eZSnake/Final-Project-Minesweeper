9f49d32c9fefe454ab4b01fb1b82a3fab8e90dc2
eZSnake <rehwalde@gmail.com>
2018-05-14 09:49:38 -0700

    Update Cell.java

--- Final-Project-Minesweeper/Cell.java
+++ Final-Project-Minesweeper/Cell.java
@@ -12,22 +12,20 @@ public class Cell {
     private boolean mine;
     private boolean revealed;
     private int numAdjacentMines;
-    private int xCords;
-    private int yCords;
 
     /* Constructor - sets up a default Cell. It is hidden, not a mine,
      *               and has 0 adjacent mines.
      */
     public Cell() {
-	     mine = MINE;
-	     numAdjacentMines = NUM_ADJ_MINES;
-       hiddenCell = HIDDEN_CELL;
+	     mine = false;
+	     numAdjacentMines = 0;
+		 revealed = false;
     }
 
     /* Mark this cell as containing a mine
      */
     public void setMine() {
-	      hiddenCell = mine;
+	      mine = true;
     }
     
     /* Helper method which returns a char value corresponding to the integer parameter
@@ -35,11 +33,7 @@ public class Cell {
      * numMines = 1 --> returns '1', numMines = 2 --> returns '2' etc
      */
     private char getNumMines(int numMines) {
-        if (int numMines == 0) {
-          return ".";
-        } else {
-          return NUM_ADJ_MINES[numMines];
-        }
+        return NUM_ADJ_MINES[numMines];
     }
 
     /* If the Cell is not revealed, returns HIDDEN_CELL
@@ -47,60 +41,40 @@ public class Cell {
      *
      */
     public char getVal() {
-	      if (cell != "#") {
-          System.out.println(getNumMines(numMines));
-        } else {
-          return HIDDEN_CELL
+	      if (revealed) {
+          	System.out.println(getNumMines(numMines));
+          } else {
+          	return HIDDEN_CELL;
+		  }  
     } 
 
     /* Mark this cell as revealed.
      */
     public void reveal() {
-	    cell = getNumMines(numMines);
+		revealed = true;
     }
 
 	/* Store the number of neighboring mines there are to this cell.
 	 */
 	  public void setAdjacentMines(int numMines) {
-        int numMines = 0;
-        if (grid[xCord][yCord + 1]) {
-          numMines++;
-        }
-        if (grid[xCords - 1][yCords + 1]) {
-          numMines++;
-        }
-        if (grid[xCords - 1][yCords]) {
-          numMines++;
-        }
-        if (grid[xCords - 1][yCords - 1]) {
-          numMines++;
-        }
-        if (grid[xCords][yCords - 1]) {
-          numMines++;
-        }
-        if (grid[xCords + 1][yCords - 1]) {
-          numMines++;
-        }
-        if (grid[xCords + 1][yCords]) {
-          numMines++;
-        }
-        if (grid[xCords + 1][yCords + 1]) {
-          numMines++;
-        }
-        return numMines;
+		  numAdjacentMines = numMines;
     }
 
 	/* Returns the number of neighboring mines there are to this cell.
 	 */
 	  public int getAdjacentMines() {
 		  int mine = 0;
-      return setAdjacentMines(int numMines);
+      	  return setAdjacentMines(int numMines);
     }
     
     /* Checks if the cell is a mine or not
      * @return   true if it is a mine, false if it is not a mine
      */
     public boolean isMine() {
-	    
+	    if (mine) {
+			return true;
+		}	else {
+			return false;
+		}
     }
 }
