cc1546ff7d98f9bb8d275963ff5c4ebf285d1903
eZSnake <rehwalde@gmail.com>
2018-05-15 09:31:23 -0700

    Add files via upload

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class Minesweeper { 
 
@@ -9,12 +8,6 @@ public class Minesweeper {
     
     // Add other static variables and constants you might need
     private static Cell[][] grid;
-    private int myRow;
-    private int myColumn;
-    private int myAmMines;
-    private int[][] minePlaces;
-    private int[][] selectedCells;
-    private int runs = 0;
 
     /* 
      * Create the grid and place mines in random locations.
@@ -27,10 +20,6 @@ public class Minesweeper {
      *      the need for ArrayOutOfBounds checking at the edges.
      */
     public static void initGrid(int rows, int columns) {
-		Cell[][] grid = new Cell[rows + 1][columns + 1];
-        int[][] selectedCells = new int[rows * columns][2];
-        myRow = rows;
-        myColumn = columns;
     }
     
     /*
@@ -39,61 +28,20 @@ public class Minesweeper {
      * @param amountMines   The number of mines to be set in the grid.
      */
     public static void disperseMines(int amountMines) {
-        myAmMines = amountMines;
-        int minePlaces[][] = new int[amountMines][2];
-        for (int i = 0; i < amountMines; i++) {
-            Random rowr = new Random();
-            Random columnc = new Random();
-            int randRow = rowr.nextInt(myRow + 1);
-            int randCol = columnc.nextInt(myColumn + 1);
-            grid[randRow][randCol] = Cell.setMine();
-            minePlaces[i][0] = randRow;
-            minePlaces[i][1] = randCol;
-        }
     }
 
     /*
      * Updates each cell with the number of adjacent cells with mines
      */
     public static void adjacentMines() {
-        int mines = 0;
-        for (int i = 0; i < myRow; i++) {
-            for (int j = 0; j < myColumn; j++) {
-                if (grid[i - 1][j - 1] == MINE) {
-                    mines++;
-                }
-                if (grid[i][j - 1] == MINE) {
-                    mines++;
-                }
-                if (grid[i + 1][j - 1] == MINE) {
-                    mines++;
-                }
-                if (grid[i + 1][j] == MINE) {
-                    mines++;
-                }
-                if (grid[i + 1][j + 1] == MINE) {
-                    mines++;
-                }
-                if (grid[i][j + 1] == MINE) {
-                    mines++;
-                }
-                if (grid[i - 1][j + 1] == MINE) {
-                    mines++;
-                }
-                if (grid[i - 1][j] == MINE) {
-                    mines++;
-                }
-                grid[i][j] = Cell.getAdjacentMines();
-            }
-        }   
     }
  
     /*
      * Method to print Minesweeper grid
      */
     private static void printGrid() {
-        for (int i = 0; i < myRow; i++) {
-            for (int j = 0; j < myColumn; j++) {
+        for (/* Your code here */) {
+            for (/* Your code here */) {
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
@@ -105,7 +53,7 @@ public class Minesweeper {
      * have been revealed.
      */
     public static void revealGrid() {
-        
+
     }
 
     /* 
@@ -130,16 +78,7 @@ public class Minesweeper {
          * Print Minesweeper grid after handling user input
          *
          */
-        for (int i = 0; i < myRow; i++){
-            for (int j = 0; j < myColumn; j++){
-                if (minePlaces[i][j] == grid[i][j]) {
-                    grid[i][j] = '*';
-                }
-                if (grid[i][j] == selectedCell[i][i]) {
-                    grid[i][j] = Cell.reveal();
-                }
-            }
-        }
+       
     }
 
     /*
@@ -147,42 +86,13 @@ public class Minesweeper {
      * @return  true if game over, false if not
      */
     public static boolean checkGameOver() {
-		for (int i = 0; i < minePlaces.length; i++) {
-			for (int j = 0; j < selectedCell.length; j++) {
-				if (selectedCell[i][0] == minePlaces[j][0] && selectedCell[i][1] == minePlaces[j][1]) {
-					return false;
-				}
-			}
-		}
-        if (myRow * myColumn - myAmMines == runs) {
-            return true;
-        } else {
-            return false;
-        }
+
     }
+
     /* Add other static methods as necessary */
     
     public static void main(String[] args) { 
-        Scanner scanner = new scanner(System.in);
-        System.out.print("What would you like the height to be? ");
-        int height = Integer.parseInt(scanner.nextLine());
-        System.out.print("What would you like the width to be? ");
-        int width = Integer.parseInt(scanner.nextLine());
-        initGrid(width, height);
-        System.out.print("How many mines would you like? ");
-        int mine = Integer.parseInt(scanner.nextLine());
-        disperseMines(mine);
-        printGrid();
-        int cellSel = 0;
-        while (!checkGameOver()) {
-            System.out.print("Select the x coordinate: ");
-            int xCord = Integer.parseInt(scanner.nextLine());
-            System.out.print("Select the y coordinate: ");
-            int yCord = Integer.parseInt(scanner.nextLine());
-            selectedCells[cellSel][0] = xCord;
-            selectedCells[cellSel][1] = yCord;
-			cellSel++;
-			runs++;
-        }
+
     }
 }
+
