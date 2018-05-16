f2aab959773a39e0ec69033b49ca0ac5adc55674
eZSnake <rehwalde@gmail.com>
2018-05-16 10:04:22 -0700

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -59,8 +59,8 @@ public class Minesweeper {
      * Method to print Minesweeper grid
      */
     private static void printGrid() {
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        for (int i = 0; i < grid[0].length; i++) {
+            for (int j = 0; j < grid.length; j++) {
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
@@ -72,7 +72,15 @@ public class Minesweeper {
      * have been revealed.
      */
     public static void revealGrid() {
-        
+        for (int i = 0; i < grid[0].length; i++) {
+            for (int j = 0; j < grid.length' j++) {
+                if (grid.getVal != HIDDEN_CELL) {
+                    
+                }
+                 
+            }
+        }
+        printGrid();
     }
 
     /* 
